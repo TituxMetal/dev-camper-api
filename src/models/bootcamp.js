9bd278a58c4729bda44ec2b4ce28777332bc1b38
Titux Metal (DEV) <github@lgdweb.fr>
2022-01-30 10:24:29 +0100

    Updating bootcamp.js file in models; renames the bootcamp model

---
+++ src/models/bootcamp.js
@@ -0,0 +1,97 @@
+import { model, Schema } from 'mongoose'
+
+const bootcampSchema = new Schema(
+  {
+    name: {
+      type: String,
+      maxlength: [50, 'Name field must be less or equal to 50 characters long'],
+      required: [true, 'Name field is required'],
+      trim: true,
+      unique: true
+    },
+    slug: String,
+    description: {
+      type: String,
+      maxlength: [500, 'Description field must be less or equal to 500 characters long'],
+      required: [true, 'Description field is required'],
+      trim: true
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
+        'Website field must be a valid url (http or https)'
+      ]
+    },
+    phone: {
+      type: String,
+      maxlength: [20, 'Phone field must be less or equal to 20 characters long']
+    },
+    email: {
+      type: String,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Email field must be a valid email']
+    },
+    address: {
+      type: String,
+      required: [true, 'Address field is required']
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ['Point']
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere'
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+      zipcode: String
+    },
+    careers: {
+      type: [String],
+      required: true,
+      enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other']
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be at least 1'],
+      max: [10, 'Rating can not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg'
+    },
+    housing: {
+      type: Boolean,
+      default: false
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+)
+
+bootcampSchema.set('toJSON', {
+  versionKey: false,
+  virtuals: true,
+  transform: (doc, { _id, ...rest }) => rest
+})
+
+const bootcamp = model('bootcamp', bootcampSchema)
+
+export default bootcamp
--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,1 @@
-/* eslint-disable import/prefer-default-export */
-export { default as Bootcamp } from './Bootcamp'
+export { default as Bootcamp } from './bootcamp'
