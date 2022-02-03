5d7deb2460f9993c5b6a6dfab43a5d239a7fb113
Titux Metal (DEV) <github@lgdweb.fr>
2022-02-03 04:25:04 +0100

    Adding bootcamp.js file in services; adds services for bootcamp CRUD operations

---
+++ src/services/bootcamp.js
@@ -0,0 +1,64 @@
+import HttpError from 'http-errors'
+
+import { Bootcamp } from '~/models'
+
+const add = async ({ name, description, address, careers, ...rest }) => {
+  if (await Bootcamp.exists({ name })) {
+    const message = { msg: 'One or more fields already exists.' }
+
+    throw new HttpError(422, { reason: Array(message) })
+  }
+
+  const newBootcamp = await Bootcamp.create({
+    name,
+    description,
+    address,
+    careers,
+    ...rest
+  })
+
+  return newBootcamp
+}
+
+const all = async (field = null) => Bootcamp.find(field)
+
+const details = async (value = '', field = '_id') => {
+  const bootcampDetails = await Bootcamp.findOne({ [field]: value })
+
+  if (!bootcampDetails) {
+    throw new HttpError(404, { reason: 'No bootcamp found.' })
+  }
+
+  return bootcampDetails
+}
+
+const remove = async (bootcampId = '') => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(bootcampId)
+
+  if (!bootcamp) {
+    throw new HttpError(404, { reason: 'No bootcamp found.' })
+  }
+
+  return bootcamp
+}
+
+const update = async (bootcampId, updatedBootcamp = {}) => {
+  if (await Bootcamp.exists({ name: updatedBootcamp.name })) {
+    const message = { msg: 'One or more fields already exists.' }
+
+    throw new HttpError(422, { reason: Array(message) })
+  }
+
+  const bootcamp = await Bootcamp.findById(bootcampId)
+
+  if (!bootcamp) {
+    throw new HttpError(404, { reason: 'No bootcamp found.' })
+  }
+
+  Object.assign(bootcamp, { ...updatedBootcamp })
+  await bootcamp.save()
+
+  return bootcamp
+}
+
+export default { add, all, details, remove, update }
---
+++ src/services/index.js
@@ -0,0 +1,1 @@
+export { default as bootcampService } from './bootcamp'
