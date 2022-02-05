84d3c3e578bae0d995b263f2705645d6c31c3de7
Titux Metal (DEV) <github@lgdweb.fr>
2022-02-05 17:10:49 +0100

    Adding course.js file in models; adds the course model

---
+++ src/models/course.js
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose'
+
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    description: { type: String, required: true, trim: true },
+    weeks: { type: Number, required: true },
+    tuition: { type: Number, required: true },
+    minimumSkill: {
+      type: String,
+      required: true,
+      enum: ['beginner', 'intermediate', 'advanced']
+    },
+    scholarshipAvailable: {
+      type: Boolean,
+      default: false
+    },
+    bootcamp: {
+      type: Schema.ObjectId,
+      ref: 'Bootcamp',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+courseSchema.set('toJSON', {
+  versionKey: false,
+  virtuals: true,
+  transform: (doc, { _id, ...rest }) => rest
+})
+
+const course = model('course', courseSchema)
+
+export default course
