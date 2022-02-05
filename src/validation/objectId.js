8cfae0fbd028b5e394680ec2c4260d5554504f57
Titux Metal (DEV) <github@lgdweb.fr>
2022-02-05 17:13:23 +0100

    Updating objectId.js file in validation; makes the objectId validator optional for parameter

--- src/validation/objectId.js
+++ src/validation/objectId.js
@@ -1,7 +1,10 @@
 import { param } from 'express-validator'
 
 const objectIdValidator = fieldName => [
-  param(fieldName).isMongoId().withMessage(`${fieldName} parameter must be a valid ObjectId.`)
+  param(fieldName)
+    .isMongoId()
+    .withMessage(`${fieldName} parameter must be a valid ObjectId.`)
+    .optional()
 ]
 
 export default objectIdValidator
